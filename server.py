c053cc7af6486cc95722db1180b2f8f16038289a
Network Engineering <neteng@faff-test.uk.wal-mart.com>
2017-06-27 14:47:05 +0100

    UDP listener and query extraction

--- server.py
+++ server.py
@@ -1,2 +1,18 @@
-from pyDNS import resolve
-print resolve("fish","goat")
+#!/usr/bin/env python3
+
+#import our modules
+import pyDNS
+import socket
+
+#set server variables
+serverIP=''
+serverPORT = 53
+
+#Open the socket
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.bind((serverIP, serverPORT))
+
+while 1:
+	data, addr = sock.recvfrom(512) # buffer size is 512 bytes
+	query = data[12:]
+	print(query)
