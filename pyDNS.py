74ee672baf4fd59f7ad06b056ba6db38e54b3e5d
simon.painter <simon.painter@gmail.com>
2017-06-28 00:36:13 +0100

    Added some parsing functions to disect the DNS Query

--- pyDNS.py
+++ pyDNS.py
@@ -1,2 +1,57 @@
-def resolve(host,type):
-	return "127.0.0.1"
+#Parse the header
+'''
+                                    1  1  1  1  1  1
+      0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                      ID                       |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |QR|   Opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    QDCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    ANCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    NSCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                    ARCOUNT                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+'''
+def parseflags(FLAGS):
+	QR = (ord(bytes(FLAGS[:1]))&128)>>7
+	Opcode = (ord(bytes(FLAGS[:1]))&120)>>3
+	AA = (ord(bytes(FLAGS[:1]))&4)>>2
+	TC = (ord(bytes(FLAGS[:1]))&2)>>1
+	RD = (ord(bytes(FLAGS[:1]))&1)
+	RA = (ord(bytes(FLAGS[1:]))&128)>>7
+	Z = (ord(bytes(FLAGS[1:]))&112)>>4
+	RCODE = (ord(bytes(FLAGS[1:]))&15)
+	return QR, Opcode, AA, TC, RD, RA, Z, RCODE
+def parseheader(header):
+	ID = header[:2]
+	FLAGS = header[2:4]
+	QR, Opcode, AA, TC, RD, RA, Z, RCODE = parseflags(FLAGS)
+	QDCOUNT = ord(bytes(header[4:5]))*256 + ord(bytes(header[5:6]))
+	ANCOUNT = ord(bytes(header[6:7]))*256 + ord(bytes(header[7:8]))
+	NSCOUNT = ord(bytes(header[8:9]))*256 + ord(bytes(header[9:10]))
+	ARCOUNT = ord(bytes(header[10:11]))*256 + ord(bytes(header[11:12]))
+	return ID, QR, Opcode, AA, TC, RD, RA, Z, RCODE,QDCOUNT, ANCOUNT, NSCOUNT, ARCOUNT
+
+#Parse the question
+'''
+                                    1  1  1  1  1  1
+      0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                                               |
+    /                     QNAME                     /
+    /                                               /
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                     QTYPE                     |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    |                     QCLASS                    |
+    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+'''
+def parsequestion(question):
+	QNAME=''
+	QTYPE=''
+	QCLASS=''
+	return QNAME,QTYPE,QCLASS
--- server.py
+++ server.py
@@ -12,7 +12,12 @@ serverPORT = 53
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.bind((serverIP, serverPORT))
 
+
+
+#loop through the query and the reply
 while 1:
-	data, addr = sock.recvfrom(512) # buffer size is 512 bytes
-	query = data[12:]
-	print(query)
+    data, addr = sock.recvfrom(512) # buffer size is 512 bytes
+    header=data[:12]
+    question = data[12:]
+    ID,QR,Opcode,AA,TC,RD,RA,Z,RCODE,QDCOUNT,ANCOUNT,NSCOUNT,ARCOUNT=pyDNS.parseheader(header)
+    QNAME,QTYPE,QCLASS = parsequestion(question)
