0b97f2e7ecff0da7bbcdb150e48f1ecc6a6f2a43
simon.painter <simon.painter@gmail.com>
2017-08-16 22:16:40 +0100

    Updated the README to hold the diagrams of the packets and moved a few things around.

--- pyDNS.py
+++ pyDNS.py
@@ -1,78 +1,18 @@
-'''
-Header
-                                    1  1  1  1  1  1
-      0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                      ID                       |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |QR|   Opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                    QDCOUNT                    |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                    ANCOUNT                    |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                    NSCOUNT                    |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                    ARCOUNT                    |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-
-Question
-                                    1  1  1  1  1  1
-      0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                                               |
-    /                     QNAME                     /
-    /                                               /
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                     QTYPE                     |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                     QCLASS                    |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-
-DNS RR response
-                                    1  1  1  1  1  1
-      0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                      ID                       |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |QR|   Opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                    QDCOUNT                    |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                    ANCOUNT                    |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                    NSCOUNT                    |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                    ARCOUNT                    |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-
-                                    1  1  1  1  1  1
-      0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                                               |
-    /                                               /
-    /                      NAME                     /
-    |                                               |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                      TYPE                     |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                     CLASS                     |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                      TTL                      |
-    |                                               |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-    |                   RDLENGTH                    |
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--|
-    /                     RDATA                     /
-    /                                               /
-    +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-'''
-
-
 class Query:
     def __init__(self,Q):
         self.header = Q[:12]
         self.question = Q[12:]
+        #Extract out the ID of the message
+        self.ID = self.header[:2]
+        #Extract the FLAGS for parsing using parseflags()
+        self.FLAGS = self.header[2:4]
+        self.QR, self.Opcode, self.AA, self.TC, self.RD, self.RA, self.Z, self.RCODE = self.parseflags(self.FLAGS)
+        #Parse additional header values
+        self.QDCOUNT = ord(bytes(self.header[4:5]))*256 + ord(bytes(self.header[5:6]))
+        self.ANCOUNT = ord(bytes(self.header[6:7]))*256 + ord(bytes(self.header[7:8]))
+        self.NSCOUNT = ord(bytes(self.header[8:9]))*256 + ord(bytes(self.header[9:10]))
+        self.ARCOUNT = ord(bytes(self.header[10:11]))*256 + ord(bytes(self.header[11:12]))
+        #Begin parsing the question
         state = 0
         expectedlength = 0
         domainstring = ''
@@ -95,15 +35,9 @@ class Query:
                 state = 1
                 expectedlength = byte
             y += 1
+        #Extract the type and class
         self.QTYPE = self.question[y:y+2]
         self.QCLASS = self.question[y+2:y+4]
-        self.ID = self.header[:2]
-        self.FLAGS = self.header[2:4]
-        self.QR, self.Opcode, self.AA, self.TC, self.RD, self.RA, self.Z, self.RCODE = self.parseflags(self.FLAGS)
-        self.QDCOUNT = ord(bytes(self.header[4:5]))*256 + ord(bytes(self.header[5:6]))
-        self.ANCOUNT = ord(bytes(self.header[6:7]))*256 + ord(bytes(self.header[7:8]))
-        self.NSCOUNT = ord(bytes(self.header[8:9]))*256 + ord(bytes(self.header[9:10]))
-        self.ARCOUNT = ord(bytes(self.header[10:11]))*256 + ord(bytes(self.header[11:12]))
 
     def parseflags(self,FLAGS):
         self.QR = (ord(bytes(FLAGS[:1]))&128)>>7
@@ -116,15 +50,18 @@ class Query:
         self.RCODE = (ord(bytes(FLAGS[1:]))&15)
         return self.QR, self.Opcode, self.AA, self.TC, self.RD, self.RA, self.Z, self.RCODE
 
-
+    def compileflags(self):
+        Flags1 = (self.QR<<7 | self.Opcode<<3 | self.AA<<2 | self.TC<<1 | self.RD)
+        Flags2 = (self.RA<<7 | self.Z<<4 | self.RCODE)
+        return Flags1.to_bytes(1, byteorder='big') + Flags2.to_bytes(1, byteorder='big')
 
     def response(self):
         #set the flag for a response
         self.QR=1
-        #set the flag for an NXDOMAIN - for testing
+        #set the flag for an NXDOMAIN and an empty answer - testing
         self.RCODE=3
+        self.answer = bytearray()
 
-        Flags1 = (self.QR<<7 | self.Opcode<<3 | self.AA<<2 | self.TC<<1 | self.RD)
-        Flags2 = (self.RA<<7 | self.Z<<4 | self.RCODE)
-        self.header = self.ID + Flags1.to_bytes(1, byteorder='big') + Flags2.to_bytes(1, byteorder='big')+self.QDCOUNT.to_bytes(2, byteorder='big')+self.ANCOUNT.to_bytes(2, byteorder='big')+self.NSCOUNT.to_bytes(2, byteorder='big')+self.ARCOUNT.to_bytes(2, byteorder='big')
-        return self.header + self.question
+        #compile the header and return the header, question and answer
+        self.header = self.ID + self.compileflags() +self.QDCOUNT.to_bytes(2, byteorder='big')+self.ANCOUNT.to_bytes(2, byteorder='big')+self.NSCOUNT.to_bytes(2, byteorder='big')+self.ARCOUNT.to_bytes(2, byteorder='big')
+        return self.header + self.question + self.answer
